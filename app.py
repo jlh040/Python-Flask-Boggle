e32eb5bfccf5d2335af3d86cede2effb2ddcdba0
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-12 18:35:23 -0700

    added a route for the game board to show up and also created the actual html game page

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/game-page')
+def show_game_page():
+    """Show the game board and put the board in the session"""
+    game_board = boggle_game.make_board()
+    session['game_board'] = game_board
+    return render_template('game-page.html', game_board = game_board)
