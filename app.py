410aac064fd337e3d653e86e15d381e7cd1a1c04
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-12 17:45:48 -0700

    added basic flask code

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '4b8z6b2x'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
