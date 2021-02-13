2d39220b2583a6de6a881e586c5108d60fdcf284
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-12 22:57:47 -0700

    the user is now notified of whether or not their word is valid

--- static/app.js
+++ static/app.js
@@ -2,16 +2,30 @@ const $guessInput = $('#guess')
 const $submitForm = $('.guess-form')
 const submitRoute = 'http://127.0.0.1:5000/submit-guess'
 
-
 $submitForm.on('submit', async function(e) {
     e.preventDefault()
+    $('h2').remove()
     let guess = $guessInput.val()
+    let msg = null
+    let $h2_message = $('<h2 class="ml-4"></h2>')
 
     data = JSON.stringify({ guess })
-    await axios({
-        method: 'post',
-        url: 'http://127.0.0.1:5000/submit-guess',
+    const response = await axios.post(
+        'http://127.0.0.1:5000/submit-guess',
         data,
-        headers: {'Content-Type': 'application/json'}
-    })
+        {headers: {'Content-Type': 'application/json'}}
+    )
+    
+    message = response.data.result
+    $h2_message.text(message)
+    if ($h2_message.text() === 'ok') {
+        $h2_message.css('color', 'green')
+    }
+    else {
+        $h2_message.css('color', 'red')
+    }
+    $h2_message.prependTo('body')
 })
+
+
+
