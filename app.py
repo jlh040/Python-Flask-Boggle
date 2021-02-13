11b1d548c6a71c5d3153656ca82554ecbb315a21
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-12 21:24:18 -0700

    now able to send json over to the /submit-guess route via our JS file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -12,6 +12,23 @@ boggle_game = Boggle()
 @app.route('/game-page')
 def show_game_page():
     """Show the game board and put the board in the session."""
-    game_board = boggle_game.make_board()
-    session['game_board'] = game_board
+    game_board = make_board()
+    set_session(game_board)
     return render_template('game-page.html', game_board = game_board)
+
+@app.route('/submit-guess', methods=['POST'])
+def handle_guess():
+    print(request.get_json()['guess'])
+    info = {'name': 'diesel'}
+    return jsonify(info)
+    # print(request.form['guess'])
+
+
+
+
+def make_board():
+    game_board = boggle_game.make_board()
+    return game_board
+
+def set_session(game_board):
+    session['game_board'] = game_board
--- static/app.js
+++ static/app.js
@@ -1,8 +1,28 @@
-const $userGuess = $('#guess')
+const $guessInput = $('#guess')
 const $submitForm = $('.guess-form')
-let formValue
+const submitRoute = 'http://127.0.0.1:5000/submit-guess'
+let guess
 
-$submitForm.on('submit', function(e) {
+$submitForm.on('submit', async function(e) {
     e.preventDefault()
-    formValue = $userGuess.val()
+    guess = $guessInput.val()
+    data = JSON.stringify({ guess })
+    await axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/submit-guess',
+        data,
+        headers: {'Content-Type': 'application/json'}
+    })
+})
+
+$submitForm.on('submit', async function(e) {
+    e.preventDefault()
+    guess = $guessInput.val()
+    data = JSON.stringify({ guess })
+    await axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/submit-guess',
+        data,
+        headers: {'Content-Type': 'application/json'}
+    })
 })
